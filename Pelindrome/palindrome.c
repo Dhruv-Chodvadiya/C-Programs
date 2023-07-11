414c2c1d725bd17f2bf67ef9a063e892201f59a3
Dhruv <chodvadiyadhruv06@gmail.com>
2023-07-11 19:34:08 +0530

    six commit

--- Pelindrome/palindrome.c
+++ Pelindrome/palindrome.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n, rem, temp;
+    int rev = 0;
+    printf("Number :");
+    scanf("%d", &n);
 
+    temp = n;
+    while (n != 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+
+    if (temp == rev)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+    getch();
+}
