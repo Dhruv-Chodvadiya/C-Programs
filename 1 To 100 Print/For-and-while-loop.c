faee191c021047951c518cd56eeb075e55fc202b
Dhruv <chodvadiyadhruv06@gmail.com>
2023-07-12 14:21:11 +0530

    Initial commit

---
+++ 1 To 100 Print/For-and-while-loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    // for (int i = 1; i <= 100; i++)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    int i = 1;
+    while (i <= 100)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+}
