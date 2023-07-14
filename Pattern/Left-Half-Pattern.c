9da76fea90e8fe7eebcd7243cbbee4fe82d5b32c
Dhruv <chodvadiyadhruv06@gmail.com>
2023-07-14 19:03:20 +0530

    Initial commit

---
+++ Pattern/Left-Half-Pattern.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int n = 5;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j <= n; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= i;k++){
+            printf("*");
+        }
+            printf("\n");
+    }
+}
